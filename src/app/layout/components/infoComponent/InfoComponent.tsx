6ed00ea85bcd90f328fa8ed6c9d331f7fad1a0da
soljic <35564870+soljic@users.noreply.github.com>
2021-06-15 14:46:15 +0200

    Bootstrap commit

---
+++ src/app/layout/components/infoComponent/InfoComponent.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./InfoComponent.css";
+import Feature_Icon_with_circle  from "../../img/Feature_Icon_with_circle.svg";
+import Logo2  from "../../img/Logo2.svg";
+import Logo3  from "../../img/Logo3.svg";
+import Logo4  from "../../img/Logo4.svg";
+import Phone_Mockup  from "../../img/Phone_Mockup.jpg";
+
+
+function InfoComponent() {
+    return (
+        <div className="container-info">
+            <div className="info">
+                <div className="div-info">
+                    <img src={Feature_Icon_with_circle} alt="" className="" /> 
+                    <h5 className="div-info-text">Create a unique Spiritus by which <br /> they will be remembered for.</h5>
+                </div>
+                <div className="div-info">
+                <img src={Logo2} alt="" className="" /> 
+                    <h5 className="div-info-text two">Post memorable photos which <br />  caption them the best.</h5>
+                </div>
+                <div className="div-info">
+                <img src={Logo3}alt="" className="" /> 
+                    <h5 className="div-info-text three">Create inspiring, memorable and <br /> immortal stories about them.</h5>
+                </div>
+                <div className="div-info">
+                <img src={Logo4} alt="" className="" /> 
+                    <h5 className="div-info-text four">That way your loved ones will never <br />  be forgotten.</h5>
+                </div>
+            </div>
+            <div className="image">
+            <img src={Phone_Mockup} alt="" className="" /> 
+            </div>
+            
+        </div>
+    )
+}
+
+export default InfoComponent
--- src/app/layout/components/navBar/NavBar.tsx
+++ src/app/layout/components/navBar/NavBar.tsx
@@ -3,22 +3,25 @@ import NavItem from "../navItem/NavItem";
 import Logo from "../logo/Logo";
 import { Button } from "antd";
 import "./NavBar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {  Container, Row, Col } from 'reactstrap';
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <>
-      <div className="navbar">
-        <Logo />
-        <div className="nav-items">
-          <NavItem name={"Stories"} />
-          <NavItem name={"About"} />
-          <NavItem name={"Contact"} />
-        </div>
-        <div className="button-navbar">
-        <Button className="button-link" type="link">Get early acces</Button>
-      </div>
-      </div>
+      <Container  className="container navbar"  fluid="md">
+        <Row >
+          <Col className="col-navbar" md={12}>
+            <Logo />
+            <a href={`/`}>Spiritus</a>
+            <Button className="button-link" type="link">
+            Get early acces
+          </Button>
+          </Col>
+          </Row>
+       
+      </Container>
     </>
   );
 }
