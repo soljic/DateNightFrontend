346719e326b5139aa5220ed49293356c939385e0
Izak Đidara <izak.didara@digacon-software.com>
2021-06-15 18:37:52 +0200

    Changing NavBar component to fix spacing

--- src/app/layout/components/navBar/NavBar.tsx
+++ src/app/layout/components/navBar/NavBar.tsx
@@ -1,29 +1,31 @@
 import React from "react";
-import NavItem from "../navItem/NavItem";
 import Logo from "../logo/Logo";
-import { Button } from "antd";
+import {Button} from "antd";
 import "./NavBar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {  Container, Row, Col } from 'reactstrap';
-import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return (
-    <>
-      <Container  className="container navbar"  fluid="md">
-        <Row >
-          <Col className="col-navbar" md={12}>
-            <Logo />
-            <a href={`/`}>Spiritus</a>
-            <Button className="button-link" type="link">
-            Get early acces
-          </Button>
-          </Col>
-          </Row>
-       
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="d-flex">
+                                <Logo/>
+                                <a href={`/`}>Spiritus</a>
+                            </div>
+                            <div>
+                                <Button className="button-link" type="link">
+                                    Get early acces
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default NavBar;
