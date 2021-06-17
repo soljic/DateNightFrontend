94183a007be6d3fe0d38fb3791e95042278d1b77
Izak Đidara <izak.didara@digacon-software.com>
2021-06-17 21:41:04 +0200

    fixing spiritus carousel

--- src/app/layout/components/spiritusCarousel/SpiritusCarousel.tsx
+++ src/app/layout/components/spiritusCarousel/SpiritusCarousel.tsx
@@ -1,29 +1,35 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, {Pagination, Navigation} from "swiper/core";
 import {SpiritusResponse} from "../../../models/SpirirtusResponse";
+
 import './SpiritusCarouselTest.css';
-// Import Swiper styles
 import 'swiper/swiper.min.css';
 
 import SliderImage1 from '../../img/slider-1.jpeg';
 import SliderImage2 from '../../img/slider-2.jpeg';
 import SliderImage3 from '../../img/slider-3.jpeg';
+import Arrow from '../../img/arrow.svg';
+
+SwiperCore.use([Pagination, Navigation]);
 
 interface Props {
     popularSpiritus: SpiritusResponse[] | undefined;
 
 }
-function SpiritusCarousel({popularSpiritus}: Props) {
 
-    // className={idx === imageIndex ? "slide activeSlide" : "slide"}
+function SpiritusCarousel({popularSpiritus}: Props) {
+    const navigationPrev = React.useRef(null);
+    const navigationNext = React.useRef(null);
+    const [index, setIndex] = useState(0);
+    console.log(index);
     return (
         <>
             <div className="slider-container">
                 <Link to="/featured">
                     <div className="fetaured-stories">FEATURED STORIES</div>
                 </Link>
-
             </div>
             <hr className="line"></hr>
             <div className="slider">
@@ -31,28 +37,55 @@ function SpiritusCarousel({popularSpiritus}: Props) {
                     <div className="row">
                         <div className="col col-8">
                             <Swiper
-                                spaceBetween={1}
+                                spaceBetween={10}
                                 slidesPerView={3}
                                 loop={true}
-                                onSlideChange={() => console.log('slide change')}
-                                onSwiper={(swiper) => console.log(swiper)}
+                                onSlideChange={(swiper) => {
+                                    setIndex(swiper.realIndex)
+                                }}
+                                onSwiper={(swiper) => {
+                                    setIndex(swiper.realIndex)
+                                }}
+                                centeredSlides={true}
+                                navigation={{
+                                    prevEl: navigationPrev.current!, // Assert non-null
+                                    nextEl: navigationNext.current!, // Assert non-null
+                                }}
+                                pagination={
+                                    {
+                                        el: '.swiper-pagination',
+                                        clickable: true,
+                                        renderBullet: (index, className) => {
+                                            return `<span class="${className}"></span>`;
+                                        }
+                                    }
+                                }
                             >
                                 <SwiperSlide>
                                     <div className="slider-image-wrap">
-                                        <img className="slider-image" src={SliderImage1}/>
+                                        <img className="slider-image" src={SliderImage1} alt="SliderImage1"/>
                                     </div>
                                 </SwiperSlide>
                                 <SwiperSlide>
                                     <div className="slider-image-wrap">
-                                        <img className="slider-image" src={SliderImage2}/>
+                                        <img className="slider-image" src={SliderImage2} alt="SliderImage1"/>
                                     </div>
                                 </SwiperSlide>
                                 <SwiperSlide>
                                     <div className="slider-image-wrap">
-                                        <img className="slider-image" src={SliderImage3}/>
+                                        <img className="slider-image" src={SliderImage3} alt="SliderImage1"/>
                                     </div>
                                 </SwiperSlide>
                             </Swiper>
+                            <div className="slider-nav">
+                                <div className="slider-arrow-left" ref={navigationPrev}>
+                                    <img src={Arrow} alt="arrow"/>
+                                </div>
+                                <div className="swiper-pagination"/>
+                                <div className="slider-arrow-right" ref={navigationNext}>
+                                    <img src={Arrow} alt="arrow"/>
+                                </div>
+                            </div>
                         </div>
                         <div className="col col-4">
                             Text
