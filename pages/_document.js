61a93667dbb26b781c139e68b969e55dc135d8af
Matija Salopek <matija.salopek@refurbed.com>
2022-06-06 13:19:54 +0200

    remove preconnects from _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin="true"
-          />
+          /> */}
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600;700&amp;display=swap"
             rel="stylesheet"
