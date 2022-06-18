dda34b952be7d0fefe8abf12f4e067f90d6c2e29
Matija Salopek <matija.salopek@refurbed.com>
2022-06-18 12:12:41 +0200

    refactor layout background settings

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ class MyDocument extends Document {
             href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
           />
         </Head>
-        <body>
+        <body class="bg-sp-dark">
           <Main />
           <NextScript />
         </body>
