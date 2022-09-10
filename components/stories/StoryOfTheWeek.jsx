5d10c12ee586892d4b40ef1eff2db9220b5f0a0e
Matija Salopek <matija.salopek994@gmail.com>
2022-09-10 18:40:00 +0200

    update homepage SOW: add bg-overlay

--- components/stories/StoryOfTheWeek.jsx
+++ components/stories/StoryOfTheWeek.jsx
@@ -1,27 +1,26 @@
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
-import { ArrowNarrowRightIcon } from "@heroicons/react/outline";
 import { ImagePlaceholder } from "../layout/Common";
 
-// there's some weird things going on:
-// - "title" from the response is ignored (it just says "STORY OF THE WEEK")
-// - "subtitle" which is just Spiritus name is used as StoryOfTheWeek.title
 export function StoryOfTheWeek({ itemId, title, imageUrl }) {
   const { t } = useTranslation("common");
 
+  // bg image with overlay is done in a messy way
   return (
-    <div key={itemId} className="rounded-sp-14 py-10">
-      <div className="relative overflow-hidden">
+    <div key={itemId} className="rounded-sp-14 my-10">
+      <div className="relative w-full overflow-hidden rounded-sp-14">
+        <div class="absolute bg-gradient-to-b from-transparent to-sp-black opacity-80 w-full h-full z-10" />
         {imageUrl ? (
           <Image
             src={imageUrl}
             alt={title}
-            className="h-full w-full object-cover rounded-lg opacity-60"
-            width={500}
-            height={500}
+            className="object-cover"
+            width={720}
+            height={520}
             layout="responsive"
-            priority // add priority since this is the first thing loaded in the UI
+            // add priority since this is the first thing loaded in the UI
+            priority
           />
         ) : (
           <div className="h-64">
@@ -29,15 +28,16 @@ export function StoryOfTheWeek({ itemId, title, imageUrl }) {
           </div>
         )}
 
-        <div className="absolute bottom-10 left-5 w-full p-4 text-white">
-          <h2 className="uppercase font-semibold">{t("story_of_the_week")}</h2>
-          <h3 className="text-4xl xl:text-4xl md:text-2xl sm:text-xl font-semibold mb-2 text">
+        <div className="absolute bottom-5 left-5 w-full p-4 text-white z-20">
+          <h2 className="uppercase font-semibold tracking-sp-tighten antialiased">
+            {t("story_of_the_week")}
+          </h2>
+          <h3 className="text-4xl xl:text-4xl md:text-2xl sm:text-xl tracking-tight font-semibold mb-4 antialiased">
             {`${title}`}
           </h3>
           <Link href={`/story-of-the-week?id=${itemId}`}>
-            <a className="inline-flex bg-sp-white text-sp-black border border-sp-black rounded-full px-5 py-3 items-center">
+            <a className="inline-flex bg-sp-white text-sp-black rounded-sp-40 px-4 py-2 items-center antialiased">
               {t("read_story")}
-              <ArrowNarrowRightIcon className="h-5 w-5 text-sp-black" />
             </a>
           </Link>
         </div>
