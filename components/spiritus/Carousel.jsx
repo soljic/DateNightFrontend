656c9c28327f2a724c5de8d801c0fe40631475ff
Matija Salopek <matija.salopek994@gmail.com>
2022-09-11 22:03:01 +0200

    update spiritus and story carousel css

--- components/spiritus/Carousel.jsx
+++ components/spiritus/Carousel.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper/core";
 import { EffectCards } from "swiper";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/outline";
 
 // import {
 //   ArrowCircleLeftIcon,
@@ -20,27 +21,43 @@ SwiperCore.use([Pagination, Navigation, Autoplay]);
 
 export function SpiritusCarousel({ images }) {
   const [index, setIndex] = useState(0);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
   return (
     !!images.length && (
-      <div className="sw-spiritus w-full mt-16 mb-14 overflow-hidden">
+      <div className="sw-spiritus relative w-full mt-16 mb-14 overflow-hidden">
+        <div className="swiper-prev-step absolute inset-y-0 left-16 z-10 h-5/6 flex items-center">
+          <button
+            ref={prevRef}
+            className="bg-white flex justify-center items-center w-9 h-9 rounded-full shadow focus:outline-none"
+          >
+            <ArrowLeftIcon className="w-5 h-5 text-black" />
+          </button>
+        </div>
         <Swiper
-          effect={'cards'}
+          onInit={(swiper) => {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+          }}
+          effect={"cards"}
           grabCursor={true}
           modules={[EffectCards]}
           onSlideChange={(swiper) => {
             setIndex(swiper.realIndex);
           }}
           slidesPerView={2}
-          onSwiper={swiper => {
-            setIndex(swiper.realIndex)
+          onSwiper={(swiper) => {
+            setIndex(swiper.realIndex);
           }}
           centeredSlides={true}
           loop={true}
           autoplay={{
             delay: 10000,
-            disableOnInteraction: false
-        }}
+            disableOnInteraction: false,
+          }}
         >
           {images.map((img, i) => {
             return (
@@ -57,6 +74,14 @@ export function SpiritusCarousel({ images }) {
             );
           })}
         </Swiper>
+        <div className="swiper-next-step absolute inset-y-0 right-16 z-10 h-5/6 flex items-center">
+          <button
+            ref={nextRef}
+            className="bg-white flex justify-center items-center w-9 h-9 rounded-full shadow focus:outline-none"
+          >
+            <ArrowRightIcon className="w-5 h-5 text-black" />
+          </button>
+        </div>
       </div>
     )
   );
