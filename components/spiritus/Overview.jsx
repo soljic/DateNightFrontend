1cefa471dfef33cf171561edfc8d79c3e95f7c73
Matija Salopek <matija.salopek994@gmail.com>
2022-10-05 12:26:10 +0200

    add location address to spiritus views

--- components/spiritus/Overview.jsx
+++ components/spiritus/Overview.jsx
@@ -8,15 +8,20 @@ export function SpiritusOverview({
   birth,
   death,
   description,
+  location,
 }) {
   return (
     <div className="w-full md:w-3/4 flex flex-col justify-between items-center py-4 mb-4">
       <div className="flex flex-col tracking-sp-tighten text-sp-black dark:text-sp-white">
         <Link href={`/spiritus/${slug}`}>
           <a className="flex flex-col items-center">
-            <button className="px-2.5 py-1 bg-gradient-to-r from-sp-day-300 to-sp-day-100 dark:from-sp-dark-brown dark:to-sp-brown text-sm font-medium rounded-full">
-              🇭🇷 Požega
-            </button>
+            {location && location?.address ? (
+              <button className="px-5 py-1 bg-gradient-to-r from-sp-day-300 to-sp-day-100 dark:from-sp-dark-brown dark:to-sp-brown text-sm font-medium rounded-full">
+                {location.address}<br></br>{location.country}
+              </button>
+            ) : (
+              <></>
+            )}
             <span className="pt-3.5 pb-2.5">
               {birth ? new Date(birth).toLocaleDateString("hr") : "?"}
               {death && ` — ${new Date(death).toLocaleDateString("hr")}`}
