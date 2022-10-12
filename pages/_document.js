1d651d95b094979786775ea08bf8c68960a63cef
Matija Salopek <matija.salopek994@gmail.com>
2022-10-12 21:40:22 +0200

    ANALYTICS: add gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -8,7 +9,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html >
+      <Html>
         <Head>
           {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
@@ -23,6 +24,23 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
+          />
+
+          <Script
+            strategy="beforeInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-2SXNVG0CD9"
+          />
+          <Script
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'G-2SXNVG0CD9');
+                  `,
+            }}
           />
         </Head>
         <body className="bg-sp-day-50 dark:bg-sp-black">
