45410d7ba1d191d058c3038854498746bafefe74
Matija Salopek <matija.salopek994@gmail.com>
2022-10-31 15:54:43 +0100

    experimetnal default language detection changes

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   i18n: {
     locales: ["en", "hr"],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "hr",
-    localeDetection: false,
+    defaultLocale: "en",
+    localeDetection: true,
     // localePath: path.resolve('./public/locales')
   },
   // react: { useSuspense: false }, //this line
